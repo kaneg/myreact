c4070b8472ad24d53e3bdd4a3973c5b9a92e4979
kaneg <gzkaneg@gmail.com>
2023-11-02 23:23:32 +0800

    update

--- src/features/fluent/App.tsx
+++ src/features/fluent/App.tsx
@@ -2,12 +2,13 @@ import {FluentProvider, teamsLightTheme} from '@fluentui/react-components';
 import {Button} from '@fluentui/react-components';
 import Nav from "../nav/Nav";
 import React from "react";
-
+import MyCard from './components/MyCard'
 export const FluentApp = () => {
     return (
         <FluentProvider theme={teamsLightTheme}>
             <Nav/>
             <Button appearance="primary">Get started</Button>
+            <MyCard/>
         </FluentProvider>
     )
 }
